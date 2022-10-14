1b8d428b4bc5f0435532792747f767259e7e5fa9
daniellenall <daniellernall@gmail.com>
2022-10-14 23:13:34 +0100

    Use context for title

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Homepage"
+        return context
     template_name = "index.html"
